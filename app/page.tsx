3cb42e9231ac5a4cd64d6247608c5f7afa6206e1
Jake <jake@blogofjake.com>
2026-07-08 10:38:17 -0400

    Ship: count now 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./page.module.css";
 
 // Update this number each time you ship a new app.
-const SHIPPED_COUNT = 2;
+const SHIPPED_COUNT = 1;
 
 export default function Home() {
   return (
